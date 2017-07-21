2bea87e9779be533d81e1b66887081f556a9ac64
emilMircea <emil.mircea30@gmail.com>
2017-07-21 14:11:21 +0200

    added create and destroy actions in the products_controller

--- app/controllers/v1/products_controller.rb
+++ app/controllers/v1/products_controller.rb
@@ -4,5 +4,28 @@ class V1::ProductsController < ApplicationController
     render json: @products, status: :ok
   end
 
+  def create
+    @product = Product.new(product_params)
+
+    @product.save
+    render json: @product, status: :created
+  end
+
+  def destroy
+    @product = Product.where(id: params[:id]).first
+    if @product.destroy
+      head(:ok)
+    else
+      head(:unproceasable_entity)
+    end
+
+
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :stock)
+  end
 
 end
